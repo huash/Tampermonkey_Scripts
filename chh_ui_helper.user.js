b37deaee80efc94d90dd812fc3c873232e19df3f
huash <huash6@sina.com>
2018-03-13 09:22:08 +0800

    hide top bar

--- chh_ui_helper.user.js
+++ chh_ui_helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         chh UI辅助工具
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.11
 // @description  try to take over the world!
 // @author       huash
 // @match        *://www.chiphell.com/forum-*
@@ -13,6 +13,6 @@
     'use strict';
     //GM_addStyle('div.ptn.xg2{display:none !important;}');
     //关闭百度分享
-    GM_addStyle('.bdshare-slide-button-box{display:none !important;}');
+    GM_addStyle('.bdshare-slide-button-box{display:none !important;}#toptb{display:none;}');
     //document.getElementById('thread_types').style.height = '';
 })();
